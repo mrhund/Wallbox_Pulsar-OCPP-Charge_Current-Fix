5cdd089e2d17eb7a39863b752cf087cee528408e
Bernhard Rastorfer <bernhard@rastorfer.net>
2024-07-30 13:45:18 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace PulsarCurrentFix
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static readonly string testData = "{\"chargeCurrent\":11,\"chargeDuration\":8640,\"chargePower\":4161,\"chargeRemainingDuration\":17725,\"chargeRemainingEnergy\":21245.176,\"chargedEnergy\":11064.282,\"chargerFeatureHeating\":false,\"chargerFeatureIntegratedDevice\":false,\"chargerIcon\":null,\"chargerPhases1p3p\":false,\"chargerPhysicalPhases\":null,\"charging\":true,\"connected\":true,\"connectedDuration\":0,\"disableThreshold\":500,\"effectiveLimitSoc\":80,\"effectiveMaxCurrent\":16,\"effectiveMinCurrent\":6,\"effectivePlanSoc\":0,\"effectivePlanTime\":null,\"effectivePriority\":0,\"enableThreshold\":-3899,\"enabled\":true,\"limitEnergy\":0,\"limitSoc\":0,\"maxCurrent\":16,\"minCurrent\":6,\"mode\":\"pv\",\"phaseAction\":\"inactive\",\"phaseRemaining\":0,\"phasesActive\":3,\"phasesConfigured\":3,\"phasesEnabled\":3,\"planEnergy\":0,\"planOverrun\":0,\"planProjectedStart\":null,\"planTime\":null,\"priority\":0,\"pvAction\":\"inactive\",\"pvRemaining\":0,\"sessionCo2PerKWh\":null,\"sessionEnergy\":11064.282,\"sessionPrice\":0.504,\"sessionPricePerKWh\":0.046,\"sessionSolarPercentage\":96.554,\"smartCostActive\":false,\"title\":\"Test\",\"vehicleClimaterActive\":null,\"vehicleDetectionActive\":false,\"vehicleLimitSoc\":0,\"vehicleName\":\"ev4\",\"vehicleOdometer\":3267,\"vehicleRange\":175,\"vehicleSoc\":47.78}\r\n";
+        private static int chargeCurrent = 0;  
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my EVCC friend!");
+            while (true)
+            {
+                if (CheckEVCCChargingAndMode() && chargeCurrent >= 6 && WallboxAPI.CheckForWrontCurrent(chargeCurrent))
+                {
+                    WallboxAPI.SetMaxCurrent(chargeCurrent);
+                }
+
+                // Warte 1 min
+                Thread.Sleep(60000);
+            }
+        }
+
+        private static bool CheckEVCCChargingAndMode()
+        {
+            try
+            {
+                string url = String.Format("{0}/api/state?jq=.result.loadpoints[{1}]",
+                    ConfigurationManager.AppSettings.Get("evcc_url"),
+                    ConfigurationManager.AppSettings.Get("evcc_loadpoint"));
+                HttpResponseMessage response = client.Send(new HttpRequestMessage(HttpMethod.Get, url));
+                response.EnsureSuccessStatusCode();
+
+                var task = Task.Run(() => response.Content.ReadAsStringAsync());
+                task.Wait();
+                var content = task.Result;
+                
+                JObject json = JObject.Parse(content);
+
+                //JObject json = JObject.Parse(testData);
+
+                JObject loadpoint = (JObject)json;
+                bool charging = loadpoint.Value<bool>("charging");
+                string mode = loadpoint.Value<string>("mode");
+                chargeCurrent = ((int)Decimal.Round(loadpoint.Value<decimal>("chargeCurrent"), 0));
+
+                if (charging == true && !mode.Equals("now")) return true;
+                else return false;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Request error: {e.Message}");
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+                return false;
+            }
+        }
+    }
+}
---
+++ WallboxAPI.cs
@@ -0,0 +1,120 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Configuration;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace PulsarCurrentFix
+{
+    internal class WallboxAPI
+    {
+        private static string tokenUrl = "https://api.wall-box.com/auth/token/user";
+        private static string chargersUrl = "https://api.wall-box.com/chargers/config/" + ConfigurationManager.AppSettings.Get("wallbox_deviceid");
+        private static string username = ConfigurationManager.AppSettings.Get("wallbox_username");
+        private static string password = ConfigurationManager.AppSettings.Get("wallbox_password");
+
+        private static string token = "";
+
+        public static void SetMaxCurrent(int maxCurrent)
+        {
+            if (token == null)
+            {
+                Console.WriteLine("Fehler beim Abrufen des JWT-Tokens");
+                return;
+            }
+
+            UpdateChargerConfig(chargersUrl, token, maxCurrent);
+        }
+
+
+        public static bool CheckForWrontCurrent(int evccChargingCurrent) {
+            try
+            {
+                // Schritt 1: Authentifizierung und Abrufen des Tokens
+                token =  GetJwtToken(tokenUrl, username, password);
+                if (token == null)
+                {
+                    Console.WriteLine("Fehler beim Abrufen des JWT-Tokens");
+                    return false;
+                }
+
+                // Schritt 2: Abrufen der Charger-Konfiguration
+                return IsWrongCurrent(chargersUrl, token, evccChargingCurrent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fehler: {ex.Message}");
+                return false;
+            }
+        }
+
+        private static string GetJwtToken(string url, string username, string password)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var authHeaderValue = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authHeaderValue);
+
+                var response = client.Send(new HttpRequestMessage(HttpMethod.Post, url));
+                if (response.IsSuccessStatusCode)
+                {
+                    var task = Task.Run(() => response.Content.ReadAsStringAsync());
+                    task.Wait();
+                    var content = task.Result;
+                    JObject json = JObject.Parse(content);
+                    return json["jwt"]?.ToString();
+                }
+                return null;
+            }
+        }
+
+        private static bool IsWrongCurrent(string url, string token, int evccChargeCurrent)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                var response = client.Send(new HttpRequestMessage(HttpMethod.Get, url));
+                if (response.IsSuccessStatusCode)
+                {
+                    var task = Task.Run(() => response.Content.ReadAsStringAsync());
+                    task.Wait();
+                    var content = task.Result;
+                    JObject json = JObject.Parse(content);
+                    var maxChargingCurrent = json.Value<int>("max_charging_current");
+                    if (maxChargingCurrent > evccChargeCurrent) return true;
+                    else return false;
+                }
+                else
+                {
+                    Console.WriteLine("Fehler beim Abrufen der Charger-Konfiguration");
+                    return false;
+                }
+            }
+        }
+
+        private static void UpdateChargerConfig(string url, string token, int maxChargingCurrent)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                var data = new { max_charging_current = maxChargingCurrent };
+                var json = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var message = new HttpRequestMessage(HttpMethod.Post, url);
+                message.Content = content;
+
+                var response = client.Send(message);
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(String.Format("Charger-Konfiguration erfolgreich aktualisiert. Neuer Wert: {0}A", maxChargingCurrent));
+                }
+                else
+                {
+                    Console.WriteLine("Fehler beim Aktualisieren der Charger-Konfiguration.");
+                }
+            }
+        }
+    }
+
+}
